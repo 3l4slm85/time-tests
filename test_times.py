2d5e81d03ea168a3fd91eb81348e8527e27c2c87
Sophie Morrison <sophie.morrison.23@ucl.ac.uk>
2023-11-02 11:24:04 +0000

    Answers UCL-COMP0233-23-24/RSE-Classwork#19

---
+++ test_times.py
@@ -0,0 +1,18 @@
+from times import compute_overlap_time, time_range
+
+def test_generic_case():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    expected = [("2010-01-12 10:30:00","2010-01-12 10:37:00"), ("2010-01-12 10:38:00", "2010-01-12 10:45:00")]
+    assert compute_overlap_time(large, short) == expected
+
+#create a test each in test_times.py for:
+#two time ranges that do not overlap
+def test_no_overlap():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 13:30:00", "2010-01-12 14:45:00")
+    assert compute_overlap_time(large, short) == 0.0
+
+#create a test each in test_times.py for:
+#two time ranges that both contain several intervals each
+#two time ranges that end exactly at the same time when the other starts
